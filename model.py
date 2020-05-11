88c68d67abd8da74c07fc740939c7948431e22d1
Nika Celan <nika.celan8@gmail.com>
2020-05-11 17:26:49 +0200

    dodala sem seznam besed in model

---
+++ model.py
@@ -0,0 +1,12 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = '+'
+NAPACNA_CRKA = '-'
+PONOVLJENA_CRKA = 'o'
+ZMAGA = 'W'
+PORAZ = 'X'
+
+class igra:
+    def __init__(self, geslo):
+
+    def __init__(self, crke):
+        
